3a2b926cd105e263b375b9f3fffdae4ad9bffb67
Daniel Krieger <dkrieger@gpiag17.gpi.kit.edu>
2018-01-24 16:04:25 +0100

    added integration test
    simple acoustic inversion. True Model was a rectangle on homogenous background. Transmission tomographic acquisition.
    At the moment integration test passes if inversion runs without errors.
    Later the Misfit will be checked against a reference value.

---
+++ par/start_Tests.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Setup test environment
+export OMP_NUM_THREADS=1
+export NUM_MPI_PROCESSES=4
+export SCAI_UNSUPPORTED=IGNORE
+
+# # Run unit tests
+# ./../bin/Tests/Test_unit
+# if [ "$?" != "0" ]; then
+# 	echo "Test failed ! "
+# 	exit 1
+# fi
+
+# Run simulation tests
+rm -rf ci/*.ci.*
+
+mpirun -np ${NUM_MPI_PROCESSES} ./../bin/IFOS ci/configuration_ci.2D.acoustic.txt
+# ./../bin/Tests/Test_CompareMisfit ci/configuration_ci.2D.acoustic.txt
+if [ "$?" != "0" ]; then
+	echo "Test failed ! "
+	return
+fi
+
