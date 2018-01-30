19c5fd4e662ba46cd5a7a5ee7e39238d666f8a64
Daniel Krieger <daniel.krieger2@student.kit.edu>
2018-01-30 14:40:54 +0100

    still merging develop into feature/develop

--- par/start_Tests.sh
+++ par/start_Tests.sh
@@ -5,23 +5,23 @@ export OMP_NUM_THREADS=1
 export NUM_MPI_PROCESSES=4
 export SCAI_UNSUPPORTED=IGNORE
 
-SOFI_EXE="./../build/bin/SOFI"
+IFOS_EXE="./../build/bin/IFOS"
 UNITTEST_EXE="./../build/bin/Test_unit"
-#INTEGRATIONTEST_EXE="./../build/bin/Test_CompareMisfit"
+INTEGRATIONTEST_EXE="./../build/bin/Test_CompareMisfit"
 
 
 # Run unit tests
-${UNITTEST_EXE}
-if [ "$?" != "0" ]; then
-	echo "Test failed ! "
-	exit 1
-fi
+#${UNITTEST_EXE}
+#if [ "$?" != "0" ]; then
+#	echo "Test failed ! "
+#	exit 1
+#fi
 
-# Run simulation tests
+ Run simulation tests
 rm -rf ci/*.ci.*
 
-mpirun -np ${NUM_MPI_PROCESSES} ${SOFI_EXE} ci/configuration_ci.2D.acoustic.txt
-# ./../bin/Tests/Test_CompareMisfit ci/configuration_ci.2D.acoustic.txt
+mpirun -np ${NUM_MPI_PROCESSES} ${IFOS_EXE} ./ci/configuration_ci.2D.acoustic.txt
+ ./../bin/Tests/Test_CompareMisfit ci/configuration_ci.2D.acoustic.txt
 if [ "$?" != "0" ]; then
 	echo "Test failed ! "
 	return
