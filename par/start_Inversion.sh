91912592073db7ea1dca7bd3ac2c792953f090db
Tilman Steinweg <tilman.metz@kit.edu>
2018-01-30 14:07:45 +0100

    added new Makefile and adjusted to FD build sys

--- par/start_Inversion.sh
+++ par/start_Inversion.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -rf seismograms/seismogram.p.mtx
 export OMP_NUM_THREADS=1
-mpirun -np 4 ./../bin/IFOS "configuration/configuration.txt"
+mpirun -np 4 ./../build/bin/IFOS "configuration/configuration.txt"
