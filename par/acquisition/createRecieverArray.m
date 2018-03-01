85cda875b05f8163b89b4b135d7eddce0ced4f09
Tilman Steinweg <tilman.metz@kit.edu>
2018-03-01 18:17:17 +0100

    updated create receiver array
    arbitrary line of receiver can be chosen now

--- par/acquisition/createRecieverArray.m
+++ par/acquisition/createRecieverArray.m
@@ -10,21 +10,28 @@ NY=100; % Total number of grid points in Y (depth)
 NZ=0; % Total number of grid points in Z
 
 % Define receiver array
-X1=20; X2=80; % X1: First receiver pos in X, X2: Last receiber pos in X
-Y1=80; Y2=80; % Y1: First receiver pos in Y, Y2: Last receiber pos in Y
-Z1=20; Z2=80; % Z1: First receiver pos in Z, Z2: Last receiber pos in Z
-NGEOPH=1; % Place a geophone on each GP (NGEOPH=1) or every other (NGEOPH=2)
+X1=80; X2=80; % X1: First receiver pos in X, X2: Last receiber pos in X
+Y1=10; Y2=89; % Y1: First receiver pos in Y, Y2: Last receiber pos in Y
+Z1=0; Z2=0; % Z1: First receiver pos in Z, Z2: Last receiber pos in Z
+NGEOPH=80; % Place a geophone on each GP (NGEOPH=1) or every other (NGEOPH=2)
+
 
 % Define receiver type
 RECEIVER_TYPE=[1]; % (1=P, 2=vX, 3=vY, 4=vZ), if you want all write [1 2 3 4]
 
-%% Create receiver array
-X=X1:NGEOPH:X2;
-Y=Y1:NGEOPH:Y2;
-Z=Z1:NGEOPH:Z2;
 
-Y=80*ones(size(X));
-Z=zeros(size(X));
+%% Create receiver array
+X=ones(1,NGEOPH);
+Y=ones(1,NGEOPH);
+Z=ones(1,NGEOPH);
+
+for ng=1:NGEOPH
+X(ng)=floor(X1+(X2-X1)/(NGEOPH-1)*(ng-1));
+Y(ng)=floor(Y1+(Y2-Y1)/(NGEOPH-1)*(ng-1));
+Z(ng)=floor(Z1+(Z2-Z1)/(NGEOPH-1)*(ng-1));
+end
+% Y=80*ones(size(X));
+% Z=zeros(size(X));
 
 numel_X=numel(X);
 RECEIVER_TYPE_vec=RECEIVER_TYPE(1)*ones(1,numel_X);
