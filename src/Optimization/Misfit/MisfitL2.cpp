260b3828cca083dda52c99a720f3683a05efff9c
Martin Pontius <martin.pontius@kit.edu>
2018-03-01 14:36:22 +0100

    Reorganised some functions and added doxygen description in misfit class(es)

--- src/Optimization/Misfit/MisfitL2.cpp
+++ src/Optimization/Misfit/MisfitL2.cpp
@@ -1,91 +1,102 @@
 #include "MisfitL2.hpp"
 #include <Acquisition/Acquisition.hpp>
 
+/*! \brief Return the L2-norm of seismograms stored in the given receiver objects (note: the misfit is summed over all components, i.e. vx,vy,vz,p)
+ *
+ *
+ \param receiversSyn Receiver object which stores the synthetic data 
+ \param receiversObs Receiver object which stores the observed data 
+ */
 template <typename ValueType>
-scai::lama::Scalar KITGPI::Misfit::MisfitL2<ValueType>::calc(KITGPI::Acquisition::Receivers<ValueType> const &receivers1, KITGPI::Acquisition::Receivers<ValueType> const &receivers2)
+scai::lama::Scalar KITGPI::Misfit::MisfitL2<ValueType>::calc(KITGPI::Acquisition::Receivers<ValueType> const &receiversSyn, KITGPI::Acquisition::Receivers<ValueType> const &receiversObs)
 {    
-    KITGPI::Acquisition::SeismogramHandler<ValueType> seismoHandler1;
-    KITGPI::Acquisition::SeismogramHandler<ValueType> seismoHandler2;
-    
-    scai::lama::Scalar misfit;
-
-    seismoHandler1 = receivers1.getSeismogramHandler();
-    seismoHandler2 = receivers2.getSeismogramHandler();
-    misfit = this->calc(seismoHandler1, seismoHandler2);
-    
-    return misfit;   
-}
-
-template <typename ValueType>
-scai::lama::Scalar KITGPI::Misfit::MisfitL2<ValueType>::calc(KITGPI::Acquisition::SeismogramHandler<ValueType> const &seismoHandler1, KITGPI::Acquisition::SeismogramHandler<ValueType> const &seismoHandler2)
-{
-    KITGPI::Acquisition::Seismogram<ValueType> seismogram1;
-    KITGPI::Acquisition::Seismogram<ValueType> seismogram2;
+        
+    KITGPI::Acquisition::Seismogram<ValueType> seismogramSyn;
+    KITGPI::Acquisition::Seismogram<ValueType> seismogramObs;
+    KITGPI::Acquisition::SeismogramHandler<ValueType> seismoHandlerSyn;
+    KITGPI::Acquisition::SeismogramHandler<ValueType> seismoHandlerObs;
     
     scai::lama::Scalar misfit;
     scai::lama::Scalar misfitSum;
     misfitSum = 0;
-    
+
+    seismoHandlerSyn = receiversSyn.getSeismogramHandler();
+    seismoHandlerObs = receiversObs.getSeismogramHandler();
+      
     /* Note that the misfit of different components (p,vx,vy,vz) is summed up. If p and v? is used at the same time, this could cause problems because they could have different scales.
        For different velocity components it should be ok. */
     
     for (int i=0; i<KITGPI::Acquisition::NUM_ELEMENTS_SEISMOGRAMTYPE; i++) {
-        seismogram1 = seismoHandler1.getSeismogram(static_cast<KITGPI::Acquisition::SeismogramType>(i));
-        seismogram2 = seismoHandler2.getSeismogram(static_cast<KITGPI::Acquisition::SeismogramType>(i));
-        misfit = this->calc(seismogram1, seismogram2);
+        seismogramSyn = seismoHandlerSyn.getSeismogram(static_cast<KITGPI::Acquisition::SeismogramType>(i));
+        seismogramObs = seismoHandlerObs.getSeismogram(static_cast<KITGPI::Acquisition::SeismogramType>(i));
+        misfit = this->calc(seismogramSyn, seismogramObs);
         misfitSum += misfit;
     }
     
-    return misfitSum;  
+    return misfitSum; 
 }
 
+/*! \brief Return the L2-norm of seismograms 
+ *
+ *
+ \param seismogramSyn Seismogram object which stores the synthetic data 
+ \param seismogramObs Seismogram object which stores the observed data 
+ */
 template <typename ValueType>
-scai::lama::Scalar KITGPI::Misfit::MisfitL2<ValueType>::calc(KITGPI::Acquisition::Seismogram<ValueType> const &seismogram1, KITGPI::Acquisition::Seismogram<ValueType> const &seismogram2)
+scai::lama::Scalar KITGPI::Misfit::MisfitL2<ValueType>::calc(KITGPI::Acquisition::Seismogram<ValueType> const &seismogramSyn, KITGPI::Acquisition::Seismogram<ValueType> const &seismogramObs)
 {    
-    /* Compare seismogram types and return error if not equal */
+    SCAI_ASSERT_ERROR(seismogramSyn.getTraceType() == seismogramObs.getTraceType(), "Seismogram types differ!");
+    
     KITGPI::Acquisition::Seismogram<ValueType> seismogramDiff;
     
-    seismogramDiff = seismogram1 - seismogram2;
+    seismogramDiff = seismogramSyn - seismogramObs;
     return 0.5*seismogramDiff.getData().l2Norm();  
 }
 
+/*! \brief Calculate the adjoint sources
+ *
+ *
+ \param adjointSources Receiver object which stores the adjoint sources (in- and output)
+ \param receiversSyn Receiver object which stores the synthetic data 
+ \param receiversObs Receiver object which stores the observed data 
+ */
 template <typename ValueType>
-void KITGPI::Misfit::MisfitL2<ValueType>::calcAdjointSources(KITGPI::Acquisition::Receivers<ValueType> &adjointSources, KITGPI::Acquisition::Receivers<ValueType> const &receivers1, KITGPI::Acquisition::Receivers<ValueType> const &receivers2)
+void KITGPI::Misfit::MisfitL2<ValueType>::calcAdjointSources(KITGPI::Acquisition::Receivers<ValueType> &adjointSources, KITGPI::Acquisition::Receivers<ValueType> const &receiversSyn, KITGPI::Acquisition::Receivers<ValueType> const &receiversObs)
 {
-    KITGPI::Acquisition::SeismogramHandler<ValueType> seismoHandler1;
-    KITGPI::Acquisition::SeismogramHandler<ValueType> seismoHandler2;
+    KITGPI::Acquisition::Seismogram<ValueType> seismogramSyn;
+    KITGPI::Acquisition::Seismogram<ValueType> seismogramObs;
+    KITGPI::Acquisition::Seismogram<ValueType> seismogramAdj;
+    KITGPI::Acquisition::SeismogramHandler<ValueType> seismoHandlerSyn;
+    KITGPI::Acquisition::SeismogramHandler<ValueType> seismoHandlerObs;
     
-    seismoHandler1 = receivers1.getSeismogramHandler();
-    seismoHandler2 = receivers2.getSeismogramHandler();
-    this->calcAdjointSources(adjointSources, seismoHandler1, seismoHandler2);
-    
-}
-
-template <typename ValueType>
-void KITGPI::Misfit::MisfitL2<ValueType>::calcAdjointSources(KITGPI::Acquisition::Receivers<ValueType> &adjointSources, KITGPI::Acquisition::SeismogramHandler<ValueType> const &seismoHandler1, KITGPI::Acquisition::SeismogramHandler<ValueType> const &seismoHandler2)
-{
-    KITGPI::Acquisition::Seismogram<ValueType> seismogram1;
-    KITGPI::Acquisition::Seismogram<ValueType> seismogram2;
+    seismoHandlerSyn = receiversSyn.getSeismogramHandler();
+    seismoHandlerObs = receiversObs.getSeismogramHandler();
     
     for (int i=0; i<KITGPI::Acquisition::NUM_ELEMENTS_SEISMOGRAMTYPE; i++) {
-        seismogram1 = seismoHandler1.getSeismogram(static_cast<KITGPI::Acquisition::SeismogramType>(i));
-        seismogram2 = seismoHandler2.getSeismogram(static_cast<KITGPI::Acquisition::SeismogramType>(i));
-        this->calcAdjointSources(adjointSources, seismogram1, seismogram2);
+        seismogramSyn = seismoHandlerSyn.getSeismogram(static_cast<KITGPI::Acquisition::SeismogramType>(i));
+        seismogramObs = seismoHandlerObs.getSeismogram(static_cast<KITGPI::Acquisition::SeismogramType>(i));
+        this->calcAdjointSeismogram(seismogramAdj, seismogramSyn, seismogramObs);
+        adjointSources.getSeismogramHandler().getSeismogram(seismogramAdj.getTraceType()) = seismogramAdj;
     }    
-
+    
 }
 
+/*! \brief Calculate the adjoint seismograms
+ *
+ *
+ \param seismogramAdj Seismogram object which stores the adjoint data  (in- and output)
+ \param seismogramSyn Seismogram object which stores the synthetic data 
+ \param seismogramObs Seismogram object which stores the observed data 
+ */
 template <typename ValueType>
-void KITGPI::Misfit::MisfitL2<ValueType>::calcAdjointSources(KITGPI::Acquisition::Receivers<ValueType> &adjointSources, KITGPI::Acquisition::Seismogram<ValueType> const &seismogram1, KITGPI::Acquisition::Seismogram<ValueType> const &seismogram2)
+void KITGPI::Misfit::MisfitL2<ValueType>::calcAdjointSeismogram(KITGPI::Acquisition::Seismogram<ValueType> &seismogramAdj, KITGPI::Acquisition::Seismogram<ValueType> const &seismogramSyn, KITGPI::Acquisition::Seismogram<ValueType> const &seismogramObs)
 {
     
-    /* Compare seismogram types and return error if not equal */
-    KITGPI::Acquisition::Seismogram<ValueType> seismogramDiff;   
-    
-    seismogramDiff = seismogram1 - seismogram2;
-    adjointSources.getSeismogramHandler().getSeismogram(seismogram1.getTraceType()) = seismogramDiff;
-    
-//     adjointSources.getSeismogramHandler().getSeismogram(seismogram1.getTraceType()).writeToFileRaw("seismograms/adjointSource.mtx");
+    SCAI_ASSERT_ERROR(seismogramSyn.getTraceType() == seismogramObs.getTraceType(), "Seismogram types differ!");
+
+    seismogramAdj = seismogramSyn - seismogramObs; 
+    /* note that the minus-sign in front of the residuals is chosen because the adjoint sources are subtracted from the wave field in the forward solver */
+//     seismogramAdj *= -1;
 
 }
 
