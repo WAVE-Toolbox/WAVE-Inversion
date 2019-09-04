0d69a82e5b84a0ebbcb46b891b3135624c6965f4
Mark <mark.wienoebst@student.kit.edu>
2019-09-04 13:20:02 +0200

    Forgot files in last commit

--- par/model/readModelfromMtx.m
+++ par/model/readModelfromMtx.m
@@ -1,10 +1,16 @@
 function [model]=readModelfromMtx(filename,NX,NY,NZ)
+% readModelfromMtx  read 1D models into 3D array
 
-fileID = fopen(filename,'r');
-HEADER = fgets(fileID);
-SIZE = fgets(fileID);
-size=str2num(SIZE);
-A=fscanf(fileID,'%e',[1 size(1)]);
-model=permute((reshape(A(:),[NX, NY, NZ])),[2 1 3]);
+%   model = readModelfromMtx(filename,NX,NY,NZ)
+%   filename: filename of the model
+%   NX,NY,NZ number of gridpoints in each direction+
+%
+%   major order of input should by X,Z,Y
+%   output is model(Y,X,Z)
+
+
+
+modelVec=readVectorfromMtx(filename);
+model=permute((reshape(modelVec(:),[NX, NZ, NY])),[3 1 2]);
 
 end
