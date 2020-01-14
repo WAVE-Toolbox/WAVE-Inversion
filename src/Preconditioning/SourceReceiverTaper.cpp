ee3530f3f8bd86757ece984abe8fa00b704a57eb
Mark <mark.wienoebst@student.kit.edu>
2020-01-14 10:25:33 +0100

    Change way of implementation for src-rec taper
    Previously dense vector values were assigned to a sparse vector. Now a
    vector assembly is used to fill the sparse vector.

--- src/Preconditioning/SourceReceiverTaper.cpp
+++ src/Preconditioning/SourceReceiverTaper.cpp
@@ -16,89 +16,80 @@ void KITGPI::Preconditioning::SourceReceiverTaper<ValueType>::apply(KITGPI::Grad
 template <typename ValueType>
 void KITGPI::Preconditioning::SourceReceiverTaper<ValueType>::init(scai::dmemo::DistributionPtr dist, scai::hmemo::ContextPtr ctx, KITGPI::Acquisition::AcquisitionGeometry<ValueType> const &Acquisition, KITGPI::Configuration::Configuration config,KITGPI::Acquisition::Coordinates<ValueType> const &modelCoordinates,IndexType radius)
 {
-    
-    lama::DenseVector<ValueType> taperTmp(dist, 0.0);
-
     /* Get local "global" indices */
-    hmemo::HArray<IndexType> globalIndices;
-    dist->getOwnedIndexes(globalIndices); // get global indeces for local part
-
-    IndexType numLocalIndices = globalIndices.size(); // Number of local indices
-
-    //temp taper arrays of local model size
-    // Harray initialized with 0.0
-    hmemo::HArray<ValueType> taperValues(numLocalIndices, 0.0, ctx);
-    hmemo::WriteAccess<ValueType> write_taperValues(taperValues);
-    ValueType write_taperValues_temp[numLocalIndices];
+    hmemo::HArray<IndexType> ownedIndeces;
+    dist->getOwnedIndexes(ownedIndeces); // get global indeces for local part
 
     //gets coordinate index for first source to be changed
     IndexType SourceCoordinate;
     
     IndexType taperType = config.get<IndexType>("sourceReceiverTaperType");
 
-    
-    //loop over all shots or receivers
-    for (IndexType srcRecNum = 0; srcRecNum < Acquisition.get1DCoordinates().size(); srcRecNum++) {
+    taper.setSameValue(dist, 1.0); // sparse vector taper gets zero element 1.0
 
-	// get 1D coordinate
-        SourceCoordinate = Acquisition.get1DCoordinates().getValue(srcRecNum);
+    lama::VectorAssembly<ValueType> assembly;
 
-        KITGPI::Acquisition::coordinate3D coord;
-        KITGPI::Acquisition::coordinate3D centerCoord;
+    // distributed acquistion coordinates needed for each processor
+    auto acquisition1DCoordinates = Acquisition.get1DCoordinates();
+    acquisition1DCoordinates.replicate();
+    auto rAcquisition1DCoordinates = scai::hmemo::hostReadAccess(acquisition1DCoordinates.getLocalValues());
 
-	// get 3D coordinate of source or receiver position
-        centerCoord = modelCoordinates.index2coordinate(SourceCoordinate);
-        ValueType distance;
-
-        hmemo::ReadAccess<IndexType> read_globalIndices(globalIndices); // Get read access to localy stored global indices
-
-	if(radius>0){
+    if(radius>0){
         //loop over all gridpoint -> this is costly maybe there is a better alternative because the size of the taper is known
-        for (IndexType i = 0; i < numLocalIndices; i++) {
+        for (IndexType ownedIndex : hmemo::hostReadAccess(ownedIndeces)){
+            ValueType valueTemp=1;
+            ValueType value=1;
+            
+            KITGPI::Acquisition::coordinate3D coord;
+            // get 3D coordinate of current ownedIndex
+            coord = modelCoordinates.index2coordinate(ownedIndex);
+                
+            //loop over all shots or receivers
+            for (IndexType srcRecNum = 0; srcRecNum < acquisition1DCoordinates.size(); srcRecNum++) {
+                // get 1D coordinate
+                SourceCoordinate = rAcquisition1DCoordinates[srcRecNum];
 
-            //(re)initialize temp with zeros
-            write_taperValues_temp[i] = 0;
-	    
-	    // get 3D coordinate of current local position i
-            coord = modelCoordinates.index2coordinate(read_globalIndices[i]);
-        
-	    // distance between src/rec position and current local gridpoint
-            distance = sqrt((centerCoord.x - coord.x) * (centerCoord.x - coord.x) + (centerCoord.y - coord.y) * (centerCoord.y - coord.y) + (centerCoord.z - coord.z) * (centerCoord.z - coord.z));
-	    
-	    // taper area:
-            if (distance <= radius) {
-                if (distance >= 1) {
-                    //-1 is needed because sparseVector=Densevector sets zero element to 0 for (grad * taper) the zero element should be 1 -> sparsevector+1  
-                    switch (taperType) {
-                        case 1:
-                            //normalized logarithmic function : log(i)/log(max) for log(i)>1 
-                            write_taperValues_temp[i] = std::log(distance) / std::log(radius) - 1;
-                            break;
-                        case 2:
-                            //cos^2 function : cos(i*pi/(2*max))^2
-                            write_taperValues_temp[i] = common::Math::pow<ValueType>(common::Math::sin<ValueType>(distance * M_PI / (2.0 * radius)), 2.0) - 1;
+                KITGPI::Acquisition::coordinate3D centerCoord;
+
+                // get 3D coordinate of source or receiver position
+                centerCoord = modelCoordinates.index2coordinate(SourceCoordinate);
+                
+                ValueType distance;
+
+                // distance between src/rec position and current local gridpoint
+                distance = sqrt((centerCoord.x - coord.x) * (centerCoord.x - coord.x) + (centerCoord.y - coord.y) * (centerCoord.y - coord.y) + (centerCoord.z - coord.z) * (centerCoord.z - coord.z));
+            
+                // taper area:
+                if (distance <= radius) {
+                    if (distance >= 1) {
+                        //-1 is needed because sparseVector=Densevector sets zero element to 0 for (grad * taper) the zero element should be 1 -> sparsevector+1  
+                        switch (taperType) {
+                            case 1:
+                                //normalized logarithmic function : log(i)/log(max) for log(i)>1 
+                                valueTemp=std::log(distance) / std::log(radius );
+                                
+                                break;
+                            case 2:
+                                //cos^2 function : cos(i*pi/(2*max))^2
+                                valueTemp=common::Math::pow<ValueType>(common::Math::sin<ValueType>(distance * M_PI / (2.0 * radius)), 2.0);
+                        }
+                        
+                        if (valueTemp < value){
+                            value=valueTemp;
+                        }
                     }
-                            
-                } else {
-                    write_taperValues_temp[i] = -1;
+                    else {
+                        value=0;
+                    }
+                    
+                assembly.push(ownedIndex,value);
                 }
-                  // update taper values only if there is no smaller tapervalue from an other source or receiver
-                if (write_taperValues_temp[i] < write_taperValues[i])
-                    write_taperValues[i] = write_taperValues_temp[i];
             }
         }
     }
-    }
-    write_taperValues.release();
-    //taper radius should be input!
-    taperTmp.setDenseValues(taperValues);
-//     lama::DenseVector<ValueType> taperTmp(dist, std::move(taperValues) ); // problems here during runtime
 
-    // copy assign tmp vector to sparsevector
     taper.setContextPtr(ctx);
-    taper = taperTmp;
-    // setting zero element to 1 by adding 1 to all values (non zero values were set by value-1)
-    taper += 1;
+    taper.fillFromAssembly(assembly);
 }
 
 template class KITGPI::Preconditioning::SourceReceiverTaper<double>;
